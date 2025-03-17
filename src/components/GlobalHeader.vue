6a0a73245c7963a094c587f20f073319606fa291
kihd <1684105955@qq.com>
2025-03-17 15:14:31 +0800

    退出登录

--- src/components/GlobalHeader.vue
+++ src/components/GlobalHeader.vue
@@ -20,7 +20,20 @@
       <a-col flex="120px">
         <div class="user-login-status">
           <div v-if="loginUserStore.loginUser.id">
-            {{ loginUserStore.loginUser.userName ?? '无名' }}
+            <a-dropdown>
+              <ASpace>
+                <a-avatar :src="loginUserStore.loginUser.userAvatar" />
+                {{ loginUserStore.loginUser.userName ?? '无名' }}
+              </ASpace>
+              <template #overlay>
+                <a-menu>
+                  <a-menu-item @click="doLogout">
+                    <LogoutOutlined />
+                    退出登录
+                  </a-menu-item>
+                </a-menu>
+              </template>
+            </a-dropdown>
           </div>
           <div v-else>
             <a-button type="primary" href="/user/login">登录</a-button>
@@ -32,10 +45,11 @@
 </template>
 <script lang="ts" setup>
 import { h, ref } from 'vue'
-import { HomeOutlined } from '@ant-design/icons-vue'
-import { MenuProps } from 'ant-design-vue'
-import { useRouter } from "vue-router";
-import {useLoginUserStore} from "@/stores/useLoginUserStore";
+import { HomeOutlined, LogoutOutlined } from '@ant-design/icons-vue'
+import { MenuProps, message } from 'ant-design-vue'
+import { useRouter } from 'vue-router'
+import { useLoginUserStore } from '@/stores/useLoginUserStore'
+import { userLogoutUsingPost } from '@/api/yonghumokuai'
 
 // 当前选中菜单
 const current = ref<string[]>([''])
@@ -71,6 +85,20 @@ router.afterEach((to, from, next) => {
 });
 
 const loginUserStore = useLoginUserStore()
+// 用户注销
+const doLogout = async () => {
+  const res = await userLogoutUsingPost()
+  console.log(res)
+  if (res.data.code === 0) {
+    loginUserStore.setLoginUser({
+      userName: '未登录',
+    })
+    message.success('退出登录成功')
+    await router.push('/user/login')
+  } else {
+    message.error('退出登录失败，' + res.data.message)
+  }
+}
 </script>
 
 <style scoped>
@@ -89,4 +117,3 @@ const loginUserStore = useLoginUserStore()
   height: 48px;
 }
 </style>
-
