aed79bb80685d8898e77e35c9bfa6bae90de6b19
kihd <1684105955@qq.com>
2025-03-19 10:36:54 +0800

    后端新增接口重启openapi脚本命令更新请求文件

--- src/api/index.ts
+++ src/api/index.ts
@@ -2,7 +2,11 @@
 /* eslint-disable */
 // API 更新时间：
 // API 唯一标识：
+import * as fileController from './fileController'
+import * as tupianmokuai from './tupianmokuai'
 import * as yonghumokuai from './yonghumokuai'
 export default {
+  fileController,
+  tupianmokuai,
   yonghumokuai,
 }
